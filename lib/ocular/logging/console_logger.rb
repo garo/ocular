b7644a14dc04367bc8f399446b04096df93b80fe
Juho Mäkinen <juho.makinen@applifier.com>
2016-03-16 12:42:47 +0200

    Add log_event dsl support

--- lib/ocular/logging/console_logger.rb
+++ lib/ocular/logging/console_logger.rb
@@ -7,7 +7,7 @@ class Ocular
 
             def initialize(settings=nil)
                 @level = Severity::DEBUG
-                @formatter = Logger::Formatter.new
+                @formatter = Formatter.new
             end            
 
             def add(severity, message = nil, run_id = nil, &block)
@@ -24,23 +24,20 @@ class Ocular
                     end
                 end
     
-                puts format_message(format_severity(severity), Time.now, run_id, message)
+                puts @formatter.format_message(severity, Time.now, message)
                 true
             end
 
-            SEV_LABEL = %w(DEBUG INFO WARN ERROR FATAL ANY).each(&:freeze).freeze
-
-            def format_severity(severity)
-                SEV_LABEL[severity] || 'ANY'
+            def log_event(property, value, run_id = nil, &block)
+                puts @formatter.format_event(property, value, Time.now, message)
+                true
             end            
 
-            def format_message(severity, datetime, progname, msg)
-                @formatter.call(severity, datetime, progname, msg)
-            end
 
             # Default formatter for log messages.
             class Formatter
-                Format = "%s, [%s#%d] %5s -- %s: %s\n".freeze
+                Format = "%s, [%s#%d] -- %s: %s\n".freeze
+                EventFormat = "[%s#%d] -- %s: %s\n".freeze
 
                 attr_accessor :datetime_format
 
@@ -48,8 +45,13 @@ class Ocular
                     @datetime_format = nil
                 end
 
-                def call(severity, time, progname, msg)
-                    Format % [severity[0..0], format_datetime(time), $$, severity, progname, msg2str(msg)]
+                def format_message(severity, time, msg)
+                    Format % [Ocular::Logging::Severity::LABELS[severity], format_datetime(time), $$, severity, msg2str(msg)]
+                end
+
+                def format_event(property, value, run_time)
+                    EventFormat % [format_datetime(time), $$, property, value]
+
                 end
 
             private
--- spec/ocular/dsl/logging_spec.rb
+++ spec/ocular/dsl/logging_spec.rb
@@ -2,6 +2,20 @@ require 'ocular'
 
 
 RSpec.describe Ocular::DSL::Logging do
+    class TestLogger
+        attr_accessor :msg
+        def initialize
+            @msg = []
+        end
+
+        def add(severity, message = nil, run_id = nil, &block)
+            @msg << [severity, message, run_id]
+        end
+
+        def log_event(property, value, run_id = nil, &block)
+            @msg << [property, value, run_id]
+        end
+    end
 
     it "#debug can be used to log a string" do
         rc = Ocular::DSL::RunContext.new(Ocular::Logging::ConsoleLogger.new)
@@ -27,6 +41,15 @@ RSpec.describe Ocular::DSL::Logging do
         rc = Ocular::DSL::RunContext.new(Ocular::Logging::ConsoleLogger.new)
         rc.fatal("fatal msg")
     end
+
+    it "#log_event can be used to log a string" do
+        l = TestLogger.new
+        rc = Ocular::DSL::RunContext.new(l)
+        rc.log_event("foo", "bar")
+        expect(l.msg[0][0]).to eq("foo")
+        expect(l.msg[0][1]).to eq("bar")
+    end
+
 
 end
 
