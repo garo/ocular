f2ce380672916d131185aa80c3b9c649133ab92f
Juho Mäkinen <juho.makinen@applifier.com>
2016-03-18 16:13:41 +0200

    Add DISABLE_ETCD env for tests

--- spec/ocular/dsl/orbit_spec.rb
+++ spec/ocular/dsl/orbit_spec.rb
@@ -21,22 +21,24 @@ RSpec.describe Ocular::DSL::Orbit do
         expect(a).not_to eq(nil)
     end
 
-    it "#orbit.get_service_endpoints works" do
-        ::Ocular::Settings.load_from_file('spec/data/settings.yaml')
+    if !ENV["DISABLE_ETCD"]
+        it "#orbit.get_service_endpoints works" do
+            ::Ocular::Settings.load_from_file('spec/data/settings.yaml')
 
-        ef = Ocular::Event::EventFactory.new
-        a = nil
-        proxy = ef.load_from_block "test_dsl" do
-            fork false
-            onEvent "name" do
-                a = orbit.get_service_endpoints("servicename")
-            end                             
+            ef = Ocular::Event::EventFactory.new
+            a = nil
+            proxy = ef.load_from_block "test_dsl" do
+                fork false
+                onEvent "name" do
+                    a = orbit.get_service_endpoints("servicename")
+                end                             
+            end
+            eventbase = proxy.events["onEvent"]["name"]
+
+            context = Ocular::DSL::RunContext.new(Ocular::Logging::ConsoleLogger.new)
+            eventbase.exec(context)
+            expect(a).not_to eq(nil)
         end
-        eventbase = proxy.events["onEvent"]["name"]
-
-        context = Ocular::DSL::RunContext.new(Ocular::Logging::ConsoleLogger.new)
-        eventbase.exec(context)
-        expect(a).not_to eq(nil)
     end
 
 end
